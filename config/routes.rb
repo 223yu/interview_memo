ea72e7833336e6cde7a6b4882df7f192b71b42bd
223yu <223yuta.jp@gmail.com>
2021-10-09 14:34:10 +0000

    #2 route setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  
   devise_for :users, controllers: {
     sessions: "users/sessions",
     passwords: "users/passwords",
     registrations: "users/registrations"
   }
+  
+  resources :answers, only: [:index, :edit, :create, :update, :destroy]
+  resources :questions, only: [:index, :new, :create]
+  resources :tags, only: [:index, :update, :destroy]
 end
