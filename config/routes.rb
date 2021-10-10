57b364215fb2ca824d655b31cb5f80baf8c614e4
223yu <223yuta.jp@gmail.com>
2021-10-10 11:49:02 +0000

    #21 fix tag follow function

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  
+
   devise_for :users, controllers: {
     sessions: "users/sessions",
     passwords: "users/passwords",
     registrations: "users/registrations"
   }
-  
+
   resources :answers, only: [:index, :edit, :create, :update, :destroy]
   resources :questions, only: [:index, :new, :create]
-  resources :tags, only: [:index, :update, :destroy]
+  resources :tags, only: [:index]
+  resources :tag_follows, only: [:create, :destroy]
 end
