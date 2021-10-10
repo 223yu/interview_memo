6e9c3419977a59178d6ba8a1f4ddcc152e528fb0
223yu <223yuta.jp@gmail.com>
2021-10-10 09:12:47 +0000

    #6 destroy function

--- app/assets/javascripts/tag.js
+++ app/assets/javascripts/tag.js
@@ -1,18 +1,26 @@
 $(document).on('turbolinks:load', function() {
   // checkを変更したときにcontrollerにアクションを送る
   $(document).on('change', '.tag__check-box', function() {
+    const checked = $(this).prop('checked');
+    const tag_id = $(this).attr('id').substr(10)
+    const token = document.getElementsByName('csrf-token')[0].content;
+    $.ajaxSetup({
+      headers: {
+        "X-CSRF-Token": token
+      }
+    });
     // checkを入れた時
-    if($(this).prop('checked') == true){
-      const tag_id = $(this).attr('id').substr(10)
-      const token = document.getElementsByName('csrf-token')[0].content;
-      $.ajaxSetup({
-        headers: {
-          "X-CSRF-Token": token
-        }
-      });
+    if(checked == true){
       $.ajax({
         url: '/tags/' + tag_id,
         type: 'PATCH',
+        dataType: 'script'
+      });
+    // checkを外した時
+    }else if(checked == false){
+      $.ajax({
+        url: '/tags/' + tag_id,
+        type: 'DELETE',
         dataType: 'script'
       });
     }
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -11,8 +11,21 @@ class TagsController < ApplicationController
     else
       flash.now[:danger] = '問題が発生しました。画面をリロードしてください。'
     end
+    respond_to do |format|
+      format.js { render 'flash.js.erb' }
+    end
   end
 
   def destroy
+    tag = Tag.find(params[:id])
+    follow_tag = FollowTag.find_by(user_id: current_user.id, tag_id: tag.id)
+    if follow_tag.destroy
+      flash.now[:success] = "#{tag.name}のフォローを解除しました"
+    else
+      flash.now[:danger] = '問題が発生しました。画面をリロードしてください。'
+    end
+    respond_to do |format|
+      format.js { render 'flash.js.erb' }
+    end
   end
 end
