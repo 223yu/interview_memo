561fa12e36507ceff9d44971f49d78d130308206
223yu <223yuta.jp@gmail.com>
2021-10-10 09:01:55 +0000

    #6 update function

--- app/assets/javascripts/tag.js
+++ app/assets/javascripts/tag.js
@@ -1,2 +1,20 @@
 $(document).on('turbolinks:load', function() {
+  // checkを変更したときにcontrollerにアクションを送る
+  $(document).on('change', '.tag__check-box', function() {
+    // checkを入れた時
+    if($(this).prop('checked') == true){
+      const tag_id = $(this).attr('id').substr(10)
+      const token = document.getElementsByName('csrf-token')[0].content;
+      $.ajaxSetup({
+        headers: {
+          "X-CSRF-Token": token
+        }
+      });
+      $.ajax({
+        url: '/tags/' + tag_id,
+        type: 'PATCH',
+        dataType: 'script'
+      });
+    }
+  });
 });
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,18 @@
 class TagsController < ApplicationController
-  
+
   def index
     @tags_hash = current_user.return_follow_status_tags
   end
-  
+
   def update
+    tag = Tag.find(params[:id])
+    if FollowTag.create(user_id: current_user.id, tag_id: tag.id)
+      flash.now[:success] = "#{tag.name}をフォローしました。"
+    else
+      flash.now[:danger] = '問題が発生しました。画面をリロードしてください。'
+    end
   end
-  
+
   def destroy
   end
 end
