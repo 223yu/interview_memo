7c29a497ba088de45366818392a52672aaa7d79d
223yu <223yuta.jp@gmail.com>
2021-10-13 05:26:11 +0000

    #41 update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 require 'csv'
 
-CSV.foreach('db/csv/user.csv', headers: true) do |row|
-  User.create!(
-    name: row['name'],
-    email: row['email'],
-    password: row['password'],
-  )
-end
+# develop環境のみ利用
+# CSV.foreach('db/csv/user.csv', headers: true) do |row|
+#   User.create!(
+#     name: row['name'],
+#     email: row['email'],
+#     password: row['password'],
+#   )
+# end
 
 CSV.foreach('db/csv/tag.csv', headers: true) do |row|
   Tag.create!(
@@ -15,7 +16,17 @@ CSV.foreach('db/csv/tag.csv', headers: true) do |row|
   )
 end
 
-CSV.foreach('db/csv/question.csv', headers: true) do |row|
+# develop環境のみ利用
+# CSV.foreach('db/csv/question.csv', headers: true) do |row|
+#   Question.create!(
+#     tag_id: row['tag_id'],
+#     body: row['body'],
+#     answer_count: row['answer_count'],
+#   )
+# end
+
+# 本番環境用
+CSV.foreach('db/csv/question(production).csv', headers: true) do |row|
   Question.create!(
     tag_id: row['tag_id'],
     body: row['body'],
@@ -23,17 +34,19 @@ CSV.foreach('db/csv/question.csv', headers: true) do |row|
   )
 end
 
-CSV.foreach('db/csv/answer.csv', headers: true) do |row|
-  Answer.create!(
-    user_id: row['user_id'],
-    question_id: row['question_id'],
-    body: row['body'],
-  )
-end
+# develop環境のみ利用
+# CSV.foreach('db/csv/answer.csv', headers: true) do |row|
+#   Answer.create!(
+#     user_id: row['user_id'],
+#     question_id: row['question_id'],
+#     body: row['body'],
+#   )
+# end
 
-CSV.foreach('db/csv/follow_tag.csv', headers: true) do |row|
-  FollowTag.create!(
-    user_id: row['user_id'],
-    tag_id: row['tag_id'],
-  )
-end
+# develop環境のみ利用
+# CSV.foreach('db/csv/follow_tag.csv', headers: true) do |row|
+#   FollowTag.create!(
+#     user_id: row['user_id'],
+#     tag_id: row['tag_id'],
+#   )
+# end
