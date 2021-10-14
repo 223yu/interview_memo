1b77006f27904e03810b16dc8c789b07c941a315
223yu <223yuta.jp@gmail.com>
2021-10-14 01:43:38 +0000

    #16 update test

--- spec/models/follow_tag_spec.rb
+++ spec/models/follow_tag_spec.rb
@@ -24,14 +24,43 @@ RSpec.describe 'フォロータグモデルに関するテスト', type: :model 
         expect(follow_tag.valid?).to eq false
       end
     end
+
+    describe '一意性のテスト' do
+      it 'user_id, tag_idの組み合わせは一意であることのテスト' do
+        create(:follow_tag, user: user, tag: tag)
+        expect(follow_tag.valid?).to eq false
+      end
+    end
   end
-  
+
   describe 'アソシエーションのテスト' do
     it 'ユーザモデルとの関係がN:1となっている' do
       expect(FollowTag.reflect_on_association(:user).macro).to eq :belongs_to
     end
     it 'タグモデルとの関係がN:1となっている' do
       expect(FollowTag.reflect_on_association(:tag).macro).to eq :belongs_to
+    end
+  end
+
+  describe 'メソッドのテスト' do
+    describe 'self.return_follow_tags_index(array)' do
+      before do
+        @user = create(:user)
+        tag_1 = create(:tag)
+        tag_2 = create(:tag, name: '新卒', genre: '状態')
+        tag_3 = create(:tag, name: '転職', genre: '状態')
+        create(:tag, name: '営業', genre: '職種')
+        create(:follow_tag, user: @user, tag: tag_1)
+        create(:follow_tag, user: @user, tag: tag_2)
+        create(:follow_tag, user: @user, tag: tag_3)
+      end
+
+      it '意図したデータが返る' do
+        array = [1,2]
+        result = []
+        result.push(FollowTag.find(1)).push(FollowTag.find(2))
+        expect(FollowTag.return_follow_tags_index(array)).to eq result
+      end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,13 +35,52 @@ RSpec.describe 'ユーザモデルに関するテスト', type: :model do
       end
     end
   end
-  
+
   describe 'アソシエーションのテスト' do
     it '回答モデルとの関係が1:Nとなっている' do
       expect(User.reflect_on_association(:answers).macro).to eq :has_many
     end
     it 'フォロータグモデルとの関係が1:Nとなっている' do
       expect(User.reflect_on_association(:follow_tags).macro).to eq :has_many
+    end
+  end
+
+  describe 'メソッドのテスト' do
+    before do
+      # テストデータの用意
+      @user = create(:user)
+      tags = []
+      tags.push(create(:tag))
+      tags.push(create(:tag, name: '新卒', genre: '状態'))
+      create(:tag, name: '転職', genre: '状態')
+      tags.push(create(:tag, name: '営業', genre: '職種'))
+      create(:tag, name: '事務', genre: '職種')
+      tags.each do |tag|
+        create(:follow_tag, user: @user, tag: tag)
+      end
+    end
+    describe 'return_follow_status_tags' do
+      it '意図したハッシュが返る' do
+        result = {
+          '状態'=>{
+            2=>{name: '新卒', status: true},
+            3=>{name: '転職', status: false}
+          },
+          '職種'=>{
+            4=>{name: '営業', status: true},
+            5=>{name: '事務', status: false}
+          }
+        }
+
+        expect(@user.return_follow_status_tags).to eq result
+      end
+    end
+
+    describe 'return_follow_tags_array' do
+      it '意図した配列が返る' do
+        result = [['一般', 1], ['新卒', 2], ['営業', 4]]
+        expect(@user.return_follow_tags_array).to eq result
+      end
     end
   end
 end
